10cc8c7b567a7ae7ce1a55a84125b6fb3904777b
rusthic <rusthic@gmail.com>
2020-02-24 20:15:49 -0300

    resolucion tarea

--- tareas-mio/clase-6/calculos.js
+++ tareas-mio/clase-6/calculos.js
@@ -1,1 +1,27 @@
- 
+function calcularMayorEdad(edades) {
+  let mayorEdad = edades[0];
+  for (let i = 0; i < edades.length; i++) {
+    if (edades[i] > mayorEdad) {
+      mayorEdad = edades[i];
+    }
+  }
+  return mayorEdad;
+}
+
+function calcularMenorEdad(edades) {
+  let menorEdad = edades[0];
+  for (let i = 0; i < edades.length; i++) {
+    if (edades[i] < menorEdad) {
+      menorEdad = edades[i];
+    }
+  }
+  return menorEdad;
+}
+
+function calcularPromedioEdades(edades) {
+  let promedio = 0;
+  for (let i = 0; i < edades.length; i++) {
+    promedio += edades[i];
+  }
+  return (promedio / edades.length).toFixed(2);
+}
--- tareas-mio/clase-6/tarea-clase-6.js
+++ tareas-mio/clase-6/tarea-clase-6.js
@@ -1,14 +1,96 @@
-document.querySelector('#siguiente-paso').onclick = function (event) {
-    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
-    const cantidadIntegrantes = Number($cantidadIntegrantes.value);
-    console.log(cantidadIntegrantes)
-            
-    event.preventDefault();
+/*
+TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad,
+la menor edad y el promedio del grupo familiar.
 
-    //a ver si andan los commits
+Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente,
+ borrando los inputs ya creados (investigar cómo en MDN).
+*/
 
-    
-    }
+document.querySelector("#siguiente-paso").onclick = function(event) {
+  const cantidadIntegrantes = Number(
+    document.querySelector("#cantidad-integrantes").value
+  );
+  borrarIntegrantesAnteriores();
+  crearIntegrantes(cantidadIntegrantes);
 
+  event.preventDefault(cantidadIntegrantes);
+};
 
+document.querySelector("#calcular").onclick = function(event) {
+  const edades = obtenerEdadesIntegrantes();
+  mostrarEdad("mayor-edad", calcularMayorEdad(edades));
+  mostrarEdad("menor-edad", calcularMenorEdad(edades));
+  mostrarEdad("promedio-edad", calcularPromedioEdades(edades));
+  mostrarResultados();
 
+  event.preventDefault();
+};
+
+document.querySelector("#resetear").onclick = resetear; //por que no anda con resetear()?
+
+function crearIntegrantes(cantidadIntegrantes) {
+  if (cantidadIntegrantes > 0) {
+    mostrarBotonCalculo();
+  } else {
+    resetear();
+  }
+  for (let i = 0; i < cantidadIntegrantes; i++) {
+    const div = document.createElement("div");
+    div.className = "integrante";
+
+    const label = document.createElement("label");
+    label.textContent = "Edad del integrante " + (i + 1) + ": ";
+    const input = document.createElement("input");
+    input.type = "number";
+
+    div.appendChild(label);
+    div.appendChild(input);
+
+    const integrante = document.querySelector("#integrantes");
+    integrantes.appendChild(div);
+  }
+}
+
+function obtenerEdadesIntegrantes() {
+  const integrantes = document.querySelectorAll(".integrante input");
+  const edades = [];
+  for (let i = 0; i < integrantes.length; i++) {
+    edades.push(Number(integrantes[i].value));
+  }
+  return edades;
+}
+
+function mostrarResultados() {
+  document.querySelector("#analisis").className = "";
+}
+
+function mostrarEdad(tipo, edad) {
+  document.querySelector(`#${tipo}`).textContent = edad;
+}
+
+function borrarIntegrantesAnteriores() {
+  const integrante = document.querySelectorAll(".integrante");
+  for (let i = 0; i < integrante.length; i++) {
+    integrante[i].remove();
+  }
+}
+
+function mostrarBotonCalculo() {
+  document.querySelector("#calcular").className = "";
+}
+
+function ocultarBotonCalculo() {
+  document.querySelector("#calcular").className = "oculto";
+}
+
+function ocultarResultados() {
+  document.querySelector("#analisis").className = "oculto";
+}
+
+function resetear() {
+  ocultarResultados();
+  borrarIntegrantesAnteriores();
+  ocultarBotonCalculo();
+}
