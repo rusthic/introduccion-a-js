60df434fd6af5611ea9c4af46cbb2c3c734b6bbc
rusthic <rusthic@gmail.com>
2021-03-15 22:47:32 -0300

    asd

--- tareas/clase-2/tarea-clase-2.js
+++ tareas/clase-2/tarea-clase-2.js
@@ -26,19 +26,16 @@ function restar(numero1, numero2) {
     return numero1 - numero2;
 }
 
-const operador = prompt('Ingrese + o -');
+const operador = prompt("Ingrese + o -");
 const numero1 = 1;
 const numero2 = 2;
 
 let resultado;
-if (operador === '+') {
+if (operador === "+") {
     resultado = sumar(numero1, numero2);
 } else {
     resultado = restar(numero1, numero2);
 }
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/template_strings
-console.log(
-    `El resultado de ${numero1} ${operador} ${numero2} es ${resultado}`
-);
-
+console.log(`El resultado de ${numero1} ${operador} ${numero2} es ${resultado}`);
