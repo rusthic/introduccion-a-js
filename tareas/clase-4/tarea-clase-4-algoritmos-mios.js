62115af6b88e2e478bc061fb0612eff21b8e1981
rusthic <rusthic@gmail.com>
2021-03-23 10:39:00 -0300

    asd

---
+++ tareas/clase-4/tarea-clase-4-algoritmos-mios.js
@@ -0,0 +1,46 @@
+//  Desafío de programación #1: Imprimí los números del 1 al 10
+
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+// Desafìo de programación  #2: Imprimí los números impares del 1 al 100
+
+// for (let i = 1; i < 100; i++) {
+//     console.log(i);
+// }
+
+// Desafío de programación #3: Imprimí la tabla de multiplicación del 7
+
+// const multiplicando = 7;
+// let resultado;
+// for (let i = 0; i <= 10; i++) {
+//     resultado = multiplicando * i;
+//     console.log(`${multiplicando} X ${i} = ${resultado}`);
+// }
+
+// Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 9
+
+// let resultado = 0;
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j <= 10; j++) {
+//         resultado = i * j;
+//         console.log(`${i} X ${j} = ${resultado}`);
+//     }
+// }
+
+// Desafío de programación #5: Calcula el resultado de la suma de los numeros del 1 al 10 en un array.
+
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function sumaNumsArray(numeros) {
+//     let resultado = 0;
+//     for (let i = 0; i < numeros.length; i++) {
+//         resultado += numeros[i];
+//     }
+//     return resultado;
+// }
+
+// console.log(sumaNumsArray(numeros));
+
+// Desafío de programación #6: Calcula 10! (10 factorial) - 10 * 9 * 8 * ... * 1
